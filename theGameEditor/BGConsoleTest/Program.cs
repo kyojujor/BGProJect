7b7339e023018c681c14237a6846dc905aaf82eb
kyojujor <524810052@qq.com>
2016-03-11 00:22:21 +0800

    提交黑金及物品成功

---
+++ theGameEditor/BGConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGService;
+
+namespace BGConsoleTest
+{
+    public class Program
+    {
+        public static readonly string url = "http://110.88.23.75:8899/mail.php";
+        public static readonly string name = "sad";
+        public static readonly int bg = 666;
+        public static readonly string itemId = "Cs_bc_002";
+
+        public static void Main(string[] args)
+        {
+            var test = BGService.MailService.SendItemAndBg(url, name, bg, itemId);
+            Console.Write(string.Format($"发送结果:{test}"));
+
+            //var temp = BGService.MailService.HttpGet("http://tech.163.com/16/0310/11/BHPURNVE00094O5H.html", "");
+            //Console.WriteLine(temp);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ theGameEditor/BGService/Service.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace BGService
+{
+    public class MailService
+    {
+        /// <summary>
+        /// 
+        /// http://110.88.23.75:8899/mail.php?name=的定位球的&bg=1&item=Cs_bc_002
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="name"></param>
+        /// <param name="bg"></param>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public static string SendItemAndBg(string address, string name, int bg = 0, string item = "")
+        {
+            if (string.IsNullOrWhiteSpace(address) || string.IsNullOrWhiteSpace(name))
+                return "端口号或角色名不能为空";
+
+            var paramsStr = "name=" + name + "&";
+            if (bg != 0)
+                paramsStr += "bg=" + bg + "&";
+            if (!string.IsNullOrWhiteSpace(item))
+                paramsStr += "item" + item + "&";
+
+            paramsStr = paramsStr.Trim('&');
+
+            return HttpGet(address, paramsStr);
+
+        }
+
+        /// <summary>
+        /// httpGet请求
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="postDataStr"></param>
+        /// <returns></returns>
+        public static string HttpGet(string Url, string postDataStr)
+        {
+            var a = Url + (postDataStr == "" ? "" : "?") + postDataStr;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(a);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("gb2312"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+    }
+}
